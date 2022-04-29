06da31eb7ab9a7d777690ee60b85a6301eb706b0
Walter Okpala <cwokpala@uwaterloo.ca>
2022-04-29 11:01:44 -0400

    connected_to database

--- myApp.js
+++ myApp.js
@@ -1,5 +1,12 @@
 require('dotenv').config();
+const mongoose = require('mongoose')
+console.log(process.env.MONGO_URI) // TODO
 
+try {
+  mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
+} catch (e) {
+  console.log(e);
+}
 
 let Person;
 
