57d704de0988390dbdef709337a5ddad7f28018a
Walter Okpala <cwokpala@uwaterloo.ca>
2022-05-01 05:48:49 -0400

    document Remove many 16

--- myApp.js
+++ myApp.js
@@ -95,7 +95,7 @@ const removeById = (personId, done) => {
 
 const removeManyPeople = (done) => {
   const nameToRemove = "Mary";
-  Person.deleteMany({name: nameToRemove}, (err , response) => {
+  Person.remove({name: nameToRemove}, (err , response) => {
     if (err) return console.error(err);
     done(null , response);
   })
