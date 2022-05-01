ef80e7a2f898fb57304c0e90a6b052e729b66d15
Walter Okpala <cwokpala@uwaterloo.ca>
2022-05-01 04:43:36 -0400

    document Remove many 8

--- server.js
+++ server.js
@@ -23,7 +23,7 @@ const enableCORS = function (req, res, next) {
     if (!process.env.XORIGIN_RESTRICT || allowedOrigins.indexOf(origin) > -1) {
       console.log(req.method);
       res.set({
-        "Access-Control-Allow-Origin": origin,
+        "Access-Control-Allow-Origin": origin || "https://www.freecodecamp.org" ,
         "Access-Control-Allow-Methods": "GET, POST, OPTIONS",
         "Access-Control-Allow-Headers":
           "Origin, X-Requested-With, Content-Type, Accept",
